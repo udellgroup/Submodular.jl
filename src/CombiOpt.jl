33d7da5dc9fe349d9bd704a5ffb93197e12cc13e
SamZhou <samman0208@hotmail.com>
2017-10-07 23:12:15 +0800

    added rank function of graphic matroids

--- src/CombiOpt.jl
+++ src/CombiOpt.jl
@@ -2,6 +2,8 @@ module CombiOpt
 
 import MathProgBase: ConicModel, loadproblem!, optimize!, numvar
 
+import LightGraphs: AbstractGraph, connected_components
+
 import Convex: AbstractExpr, Variable, Constraint, Constant, Solution, Problem, convert
 import Convex: Vexity, ConstVexity, AffineVexity, ConvexVexity, ConcaveVexity, NotDcp
 import Convex: Sign, ComplexSign, Monotonicity, Nonincreasing, Nondecreasing, NoMonotonicity
@@ -39,6 +41,7 @@ include("continuous_sets/poly.jl")
 
 # combinatorial functions
 include("combinatorial_functions/card.jl")
+include("src/combinatorial_functions/rank_of_graph_matroid.jl")
 include("combinatorial_functions/modular.jl")
 
 # models
