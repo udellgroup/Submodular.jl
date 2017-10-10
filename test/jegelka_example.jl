9ae7e85754d181b0c95466d4509c4a65251d128c
swati1729 <swatig@mit.edu>
2017-10-09 19:17:51 -0700

    modified example

--- test/jegelka_example.jl
+++ test/jegelka_example.jl
@@ -1,14 +1,16 @@
 using HDF5, JLD
 using Distributions
 
+#constructing a random bipartite graph (U = N, V= N) with degree varying from 
+#~0.15 N to ~0.65 N (effectively we disregard overlapping edges). Higher degree will
+#result is a greater overlap and the submodular function will be further from being 
+#monotone. 
+
 N=100; 
-
 U_incid = Array{Array}(N);
-
 for i=1:N
 	U_incid[i] = []
 end
-
 for i = 1:N
 		degree_of_i = 0.15*N + Int(round(rand()*N/2));
 		for j = 1:degree_of_i
@@ -17,20 +19,18 @@ for i = 1:N
 		end
 end
 
-m = 100*rand(N,1);
-weight = 100*rand(N, N);
-
+#assign random weights to vertices on both sides of the bipartition
+m = 100*rand(N,1); #for vertices in U
+weight = 100*rand(N, 1); #for vertices in V
 
 function jegelka_example(S)
-	val = 0;
+	val = sum(m[S]);
+	neighborhood = [];
 	for i in S 
-		val = val + m(i);
-		w = 0;
-		for j in U_incid(i)
-			w = w + weight(i, j);
-		end
-		val = val + sqrt(w);
+			neighborhood = vcat(neighborhood, U_incid[i]) #compute the neighborhood of the selection of vertices from U
 	end
+	neigborhood = unique(neighborhood);
+	val = val + 10*sqrt(sum(weight[neighborhood])); # one can vary the constant in front of the weight of the neighborhood as well. 
 	return val;
 end
 
