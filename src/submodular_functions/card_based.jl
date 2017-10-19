51f8796a1113766e811f7e0e8199c70fc228a130
SamZhou <samman0208@hotmail.com>
2017-10-19 18:46:25 +0800

    changed src/combinatorial_functions/ to src/submodular_functions

---
+++ src/submodular_functions/card_based.jl
@@ -0,0 +1,83 @@
+#############################################################################
+# card.jl
+# Handles functions of the form f(A) = g(length(A)), where g is a concave function
+#############################################################################
+
+export card, compose
+export in
+export sign, monotonicity, modularity, evaluate
+
+type CardBasedAtom <: CombiFunc
+  head::Symbol
+  id_hash::UInt64
+  children::Tuple{AbstractExpr}
+  size::Tuple{Int, Int}
+  setvariables::Array{SetVariable}
+
+  function CardBasedAtom(x::Function, S::CombiSet)
+    z = Variable(1)
+    children = (x(z),)                           # create an expression
+    if evaluate(x(z), 0)[1] != 0
+      error("A function has to return 0 at 0 in order to derive a cardinality-based function.")
+    end
+    setvariables = get_sv(S)
+    return new(:card, hash(children), children, (1, 1), setvariables)
+  end
+end
+
+card(f::Function, S::CombiSet) = CardBasedAtom(f, S)
+
+card(S::CombiSet) = card(x -> x, S)
+
+function sign(x::CardBasedAtom)
+  return sign(x.children[1])
+end
+
+function monotonicity(x::CardBasedAtom)
+  return (monotonicity(x.children[1]), )
+end
+
+function modularity(x::CardBasedAtom)
+  if vexity(x.children[1]) == ConcaveVexity()
+    return SubModularity()
+  elseif vexity(x.children[1]) == ConvexVexity()
+    return SuperModularity()
+  elseif vexity(x.children[1]) == AffineVexity()
+    return Modularity()
+  elseif vexity(x.children[1]) == ConstVexity()
+    return ConstModularity()
+  else
+    return NotDetermined()
+  end
+end
+
+function evaluate(x::CardBasedAtom)
+  var = x.setvariables[1]
+  var1 = get_v(x.children[1])
+  var1[1].value = length(Set(get_elements(var)))
+  evaluate(x.children[1])
+end
+
+function in(w::AbstractArray, p::SubmodPoly{CardBasedAtom})
+  n = length(w)
+  @assert length(p.V) == n
+  checker = true
+  S = sort(p.V)
+  ordering = sortperm(w, rev = true)
+  h = 0
+  ind = []
+  y = zeros(n + 1)
+  for i = 1:length(w)
+    y[i + 1] = evaluate(p.f, S[push!(ind, ordering[i])])[1]
+    h += - y[i] - w[i] + y[i + 1]
+    if h < 0
+      checker = false
+      break
+    end
+  end
+  return checker
+end
+
+function compose(f::Function, g::CardBasedAtom)
+  return card(f, g.setvariables[1])
+end
---
+++ src/submodular_functions/rank_of_graph_matroid.jl
@@ -0,0 +1,47 @@
+#############################################################################
+# rank_of_graph_matroid.jl
+# given a graph G = (V, E)
+# f(S) = n - # of connected components of the subgraph of G induced by S
+#############################################################################
+
+export rank_of_graph_matroid
+export sign, monotonicity, modularity, evaluate
+
+type RankOfGraphicMatroidAtom <: CombiFunc
+  head::Symbol
+  id_hash::UInt64
+  children::Tuple{AbstractGraph, CombiSet}
+  size::Tuple{Int, Int}
+  graph::AbstractGraph
+  setvariables::Array{CombiSet}
+
+  function RankOfGraphicMatroidAtom(g::AbstractGraph, S::CombiSet)
+    if nv(g) != S.cardinality
+      error("Cannot define a modular function when the vector's size is different from the (card(baseset), 1).")
+    else
+      children = (g, S)
+      setvariables = get_sv(S)
+      return new(:rankofgraphmatroid, hash(children), children, (1, 1), g, setvariables)
+    end
+  end
+end
+
+rank_of_graph_matroid(g::AbstractGraph, S::CombiSet) = RankOfGraphicMatroidAtom(g, S)
+
+function sign(x::RankOfGraphicMatroidAtom)
+  return Positive()
+end
+
+function monotonicity(x::RankOfGraphicMatroidAtom)
+  return (Nonincreasing(), )
+end
+
+function modularity(x::RankOfGraphicMatroidAtom)
+  return SubModularity()    # up to verification
+end
+
+function evaluate(x::RankOfGraphicMatroidAtom)
+  sub_graph, vmap = induced_subgraph(x.children[1], get_elements(x.children[2]))
+  val = x.children[2].cardinality - length(connected_components(sub_graph))
+  return val
+end
